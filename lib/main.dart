0b462480c13c702ce6b25d813e6346248e4736a8
mepha89 <umamarahman12@gmail.com>
2023-10-20 00:24:08 -0230

    UR-004-setup-main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,20 @@
 import 'package:dear_diary_app/model/diary_entry_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'boxes.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+import 'view/diary_log_view.dart';
+import 'package:dear_diary_app/controller/diary_controller.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
   Hive.registerAdapter(DiaryEntryAdapter());
 
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  Box box = await Hive.openBox<DiaryEntry>('diary_entries');
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(MaterialApp(
+    title: 'Dear Diary App',
+    home: DiaryLogView(),
+  ));
 }
