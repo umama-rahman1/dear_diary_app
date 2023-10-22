cd7d4544c208775db26d69c55f8a48a3b5e64943
mepha89 <umamarahman12@gmail.com>
2023-10-22 13:47:02 -0230

    UR-008-add-theme-to-app

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData appTheme = ThemeData(
+  primarySwatch: Colors.blue,
+  scaffoldBackgroundColor: Colors.amber,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  textTheme: GoogleFonts.deliusTextTheme(),
+);
