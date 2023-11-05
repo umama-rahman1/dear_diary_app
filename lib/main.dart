0c4d36ffc6df4bbc2b9ab0d9bc3b52e23810f076
mepha89 <umamarahman12@gmail.com>
2023-11-05 14:37:05 -0330

    UR-021-init-firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,19 @@ import 'package:dear_diary_app/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'view/diary_log_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'view/welcome_page_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
+  await Hive.initFlutter();
   Hive.registerAdapter(DiaryEntryAdapter());
 
   Box box = await Hive.openBox<DiaryEntry>('diaryEntries');
