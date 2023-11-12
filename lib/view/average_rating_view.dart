3f001494a3c9d1677105483a44b8a76f8d7f74f6
mepha89 <umamarahman12@gmail.com>
2023-11-12 18:43:06 -0330

    UR-020-statistics-work

--- lib/view/average_rating_view.dart
+++ lib/view/average_rating_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:dear_diary_app/model/diary_entry_model.dart';
-import 'package:dear_diary_app/controller/diary_controller.dart';
+import 'package:dear_diary_app/diary_firestore_model/diary_entry_model.dart';
+// import 'package:dear_diary_app/controller/diary_controller.dart';
+import 'package:dear_diary_app/controller/diary_entry_service.dart';
 
 class AverageRatingView extends StatefulWidget {
   @override
@@ -9,35 +10,41 @@ class AverageRatingView extends StatefulWidget {
 }
 
 class _AverageRatingViewState extends State<AverageRatingView> {
-  final DiaryController _diaryController = DiaryController();
-  late List<DiaryEntry> diaryEntries;
+  final DiaryEntryService _diaryEntryService = DiaryEntryService();
+  late Stream<List<DiaryEntry>> diaryEntriesStream;
   Map<String, double> averageRatings = {};
 
   @override
   void initState() {
     super.initState();
+    diaryEntriesStream = _diaryEntryService.getUsersDiaryEntries();
     _calculateAverageRatings();
   }
 
   void _calculateAverageRatings() {
-    // Retrieve all diary entries
-    diaryEntries = _diaryController.getAllDiaryEntries();
-
-    // Calculate average ratings for each month
-    for (var entry in diaryEntries) {
-      final dateFormat = DateFormat('yyyy-MM');
-      final month = dateFormat.format(entry.date);
-      if (averageRatings.containsKey(month)) {
-        averageRatings[month] = (averageRatings[month]! + entry.rating) / 2.0;
-      } else {
-        averageRatings[month] = entry.rating.toDouble();
+    diaryEntriesStream.listen((List<DiaryEntry> entries) {
+      // Calculate average ratings for each month
+      for (var entry in entries) {
+        final dateFormat = DateFormat('yyyy-MM');
+        final month = dateFormat.format(entry.date);
+        if (averageRatings.containsKey(month)) {
+          averageRatings[month] = (averageRatings[month]! + entry.rating) / 2.0;
+        } else {
+          averageRatings[month] = entry.rating.toDouble();
+        }
       }
-    }
+
+      // Update the UI
+      if (mounted) {
+        setState(() {});
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final sortedMonths = averageRatings.keys.toList()..sort((a, b) => b.compareTo(a));
+    final sortedMonths = averageRatings.keys.toList()
+      ..sort((a, b) => b.compareTo(a));
 
     return Scaffold(
       appBar: AppBar(
@@ -49,7 +56,8 @@ class _AverageRatingViewState extends State<AverageRatingView> {
           final month = sortedMonths[index];
           final averageRating = averageRatings[month];
 
-          final formattedMonth = DateFormat('MMMM yyyy').format(DateFormat('yyyy-MM').parse(month));
+          final formattedMonth = DateFormat('MMMM yyyy')
+              .format(DateFormat('yyyy-MM').parse(month));
 
           return ListTile(
             title: Text(
