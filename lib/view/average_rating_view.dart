5b8ee2b79825d25134e3b9aef63aa0c6aa851f73
mepha89 <umamarahman12@gmail.com>
2023-11-12 18:49:03 -0330

    UR-020-barchart-works

--- lib/view/average_rating_view.dart
+++ lib/view/average_rating_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:dear_diary_app/diary_firestore_model/diary_entry_model.dart';
-// import 'package:dear_diary_app/controller/diary_controller.dart';
 import 'package:dear_diary_app/controller/diary_entry_service.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class AverageRatingView extends StatefulWidget {
   @override
@@ -50,22 +50,41 @@ class _AverageRatingViewState extends State<AverageRatingView> {
       appBar: AppBar(
         title: Text('Average Star Ratings by Month'),
       ),
-      body: ListView.builder(
-        itemCount: sortedMonths.length,
-        itemBuilder: (context, index) {
-          final month = sortedMonths[index];
-          final averageRating = averageRatings[month];
-
-          final formattedMonth = DateFormat('MMMM yyyy')
-              .format(DateFormat('yyyy-MM').parse(month));
-
-          return ListTile(
-            title: Text(
-              'Average Rating for $formattedMonth = ${averageRating?.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BarChart(
+          BarChartData(
+            barGroups: sortedMonths.map((month) {
+              final averageRating = averageRatings[month] ?? 0.0;
+              return BarChartGroupData(
+                x: sortedMonths.indexOf(month),
+                barRods: [
+                  BarChartRodData(
+                    y: averageRating,
+                    colors: [Colors.blue],
+                    width: 20.0,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ],
+                showingTooltipIndicators: [0],
+              );
+            }).toList(),
+            titlesData: FlTitlesData(
+              leftTitles: SideTitles(showTitles: true),
+              bottomTitles: SideTitles(
+                showTitles: true,
+                getTitles: (double value) {
+                  if (value.toInt() < 0 ||
+                      value.toInt() >= sortedMonths.length) {
+                    return '';
+                  }
+                  return sortedMonths[value.toInt()];
+                },
+              ),
             ),
-          );
-        },
+            borderData: FlBorderData(show: true),
+          ),
+        ),
       ),
     );
   }
