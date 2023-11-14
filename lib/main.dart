54e0526de35c81838c6234a1de170663a18969f3
mepha89 <umamarahman12@gmail.com>
2023-11-13 20:32:26 -0330

    UR-016-update-main-to-be-stateful

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,35 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MaterialApp(
-    title: 'Dear Diary App',
-    theme: appTheme,
-    home: const DiaryLoginView(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+
+  /// InheritedWidget style accessor to our State object.
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dear Diary App',
+      theme: appTheme,
+      darkTheme: appThemeDark,
+      themeMode: _themeMode,
+      home: const DiaryLoginView(),
+    );
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
 }
